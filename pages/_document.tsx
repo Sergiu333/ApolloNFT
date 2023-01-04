fe81b6b1e6abb5bdf121c5f30e13bfe64c88969e
Sergiu <sergiu.anghelenici@corlab.tech>
2023-01-04 12:16:36 +0200

    added title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>ApolloNFT</title>
           <meta name="apple-mobile-web-app-title" content="ApolloNFT" />
           <meta name="application-name" content="ApolloNFT" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
